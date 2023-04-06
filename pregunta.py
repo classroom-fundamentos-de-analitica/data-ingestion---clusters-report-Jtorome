fad397ba3619198d08563b8a64cdb1656ffd29bc
Juan Alberto Toro Mejia <juanalbtoro@hotmail.com>
2023-04-05 23:05:47 -0500

    finished

--- pregunta.py
+++ pregunta.py
@@ -13,9 +13,40 @@ import pandas as pd
 
 
 def ingest_data():
+    file = open('clusters_report.txt', mode='r')
+    bad_titles = file.readline().split("  ")
 
-    #
-    # Inserte su código aquí
-    #
+    for _ in range(3): file.readline()
 
+    titles = []
+    for bt in bad_titles:
+        bt_strip = bt.strip()
+        if bt_strip:
+            if bt_strip[len(bt_strip)-2:] == 'de':
+                bt_strip+=' palabras clave'
+            titles.append(bt_strip.replace(' ', '_').lower())
+
+    data_to_df = []
+    row = []
+    line = file.readline()
+    while line:
+        if line.count('%'):
+            with_dspaces = line.strip().replace('%', '').replace('  ', ' ')
+            while '  ' in with_dspaces:
+                with_dspaces = with_dspaces.replace('  ',' ')
+            row = with_dspaces.split(' ')
+        elif line.strip() == '':
+            data_to_df.append([
+                int(row[0]),
+                int(row[1]),
+                float(row[2].replace(",",".")),
+                " ".join(row[3:])
+            ])
+        else:
+            with_dspaces = line.strip().replace(".","")
+            while '  ' in with_dspaces:
+                with_dspaces = with_dspaces.replace('  ',' ')
+            row += with_dspaces.split(' ')
+        line = file.readline()
+    df = pd.DataFrame(data_to_df, columns=titles)
     return df
